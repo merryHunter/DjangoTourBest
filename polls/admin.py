cd8f09280b7e0851ca6a374cef3a07e736d51d3d
merryHunter <chernuhaiv@gmail.com>
2016-01-01 18:34:00 +0200

    init, dashboard present

---
+++ polls/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Question, Choice, UserProfile
+
+admin.site.register(Question)
+admin.site.register(Choice )
+admin.site.register(UserProfile)
